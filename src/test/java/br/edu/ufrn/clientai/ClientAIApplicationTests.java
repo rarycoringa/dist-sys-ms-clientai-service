05b60611a7b5af9dfd0d367623ffc9b3e6e59e70
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-07-13 22:05:04 -0300

    create first structure of application

---
+++ src/test/java/br/edu/ufrn/clientai/ClientAIApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.clientai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
